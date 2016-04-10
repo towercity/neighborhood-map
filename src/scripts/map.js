52d97857eec7650d69c5d4ffe4575670cbbed5a4
Matthew Nerger <mnerger@lsu.edu>
2016-04-10 14:09:15 -0500

    Update map settings

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,10 +1,11 @@
 console.log("linked!");
 
 function initMap() {
-  // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map-div'), {
-    center: {lat: -34.397, lng: 150.644},
-    scrollwheel: false,
-    zoom: 8
-  });
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('map-div'), {
+        center: {lat: 27.954, lng: -82.459},
+        scrollwheel: false,
+        mapTypeControl: false,
+        zoom: 10
+    });
 }
