5c8a88cf5c4993b3ab3d22a52728618b5897b9ba
Matthew Nerger <mnerger@lsu.edu>
2016-04-10 19:25:53 -0500

    Change map focus

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,9 +1,9 @@
 function initMap() {
     // Create a map object and specify the DOM element for display.
     var map = new google.maps.Map(document.getElementById('map-div'), {
-        center: {lat: 27.954, lng: -82.459},
+        center: {lat: 27.865, lng: -82.459},
         scrollwheel: false,
         mapTypeControl: false,
-        zoom: 10
+        zoom: 9
     });
 }
