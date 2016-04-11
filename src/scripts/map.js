52e756931dd91df06143fe01548ecd63a209b996
Matthew Nerger <mnerger@lsu.edu>
2016-04-10 19:14:02 -0500

    Style top search bar

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,5 +1,3 @@
-console.log("linked!");
-
 function initMap() {
     // Create a map object and specify the DOM element for display.
     var map = new google.maps.Map(document.getElementById('map-div'), {
