79a299e6109d22dd1ac696b2346b597a50edfcd6
Matthew Nerger <mnerger@lsu.edu>
2016-04-20 17:31:57 -0500

    Alter how markers are placed on the map to fit use of address rather than latlng

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -9,39 +9,68 @@ function initMap() {
     map = new google.maps.Map(mapDiv, {
         center: {lat: 25.721, lng: -80.2777},
         mapTypeControl: false,
-        zoom: 12,
-        geocoder: new google.maps.Geocoder(),
-        methods: {
-            reenter: function(latLang) {
-                map.setCenter(latLang);
-            }
-        }
+        zoom: 12
     });
+
+    function locationFinder() {
+        var locations = [];
+
+        locationArray.forEach(function(place) {
+            locations.push(place.address);
+        });
+
+        return locations;
+    }
+
+    function createMapMarker(placeData) {
+        var lat = placeData.geometry.location.lat();
+        var lon = placeData.geometry.location.lng();
+        var name = placeData.formatted_address;
+
+        var marker = new google.maps.Marker({
+          map: map,
+          position: placeData.geometry.location,
+          name: name
+        });
+
+        marker.addListener('click', function() {
+            map.setZoom(15);
+            map.panTo(placeData.geometry.location);
+            //my.vm.clickLocation(place);
+        });
+    }
+
+    function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            createMapMarker(results[0]);
+        }
+    }
+
+    function pinMarkers(locations) {
+        var service = new google.maps.places.PlacesService(map);
+
+        locations.forEach(function(place) {
+            var request = {
+                query: place
+            };
+            service.textSearch(request, callback);
+        });
+    }
 
     // Waits till whole page is loaded to ensure vars from other files are available
     $(document).ready(function() {
-        // Iterate through locationArray and add markers to the map
-        locationArray.forEach(function(place) {
-            map.geocoder.geocode({'address': place.address}, function(results, status) {
-                if (status === google.maps.GeocoderStatus.OK){
-                    var marker = new google.maps.Marker({
-                        position: results[0].geometry.location,
-                        map: map
-                    });
+        var locations = locationFinder();
 
-                    marker.addListener('click', function() {
-                        map.setZoom(13);
-                        map.panTo(marker.getPosition());
-                        my.vm.clickLocation(place);
-                    });
-                } else {
-                    console.log(status);
-                }
+        pinMarkers(locations);
+
+        /*
+        locationArray.forEach(function(place) {
+            marker.addListener('click', function() {
+                map.setZoom(13);
+                map.panTo(place.coordinates);
+                my.vm.clickLocation(place);
             });
         });
-    });
-
-    google.maps.event.addDomListener(mapDiv, 'click', function() {
-        //window.alert('Map was clicked!');
+        */
     });
 };
